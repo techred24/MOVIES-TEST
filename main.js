642d94019e64fde1e72797c3160317d83cade0c4
Rafael Temich <f.rafael.temich24@gmail.com>
2023-07-06 16:48:12 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const URL_BASE = 'https://api.themoviedb.org/3'
+
+const API_KEY = '?api_key=c5190f83a95bc72ce01d58972d1c196a';
+//https://api.themoviedb.org/3/movie/popular
+// const response = await fetch(`${URL_BASE}/movie/popular${API_KEY}`);
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNTE5MGY4M2E5NWJjNzJjZTAxZDU4OTcyZDFjMTk2YSIsInN1YiI6IjYyM2E2ZGQ0MTNhZjVmMDA0NzE0YmE1OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.w29ToiHMMDrrbl7vjE5Nj5SxSQceSGogIDSfTrDVsXg'
+    }
+}
+const getRequest2 = async () => {
+    const response = await fetch(`${URL_BASE}/movie/popular`, options);
+    const json = await response.json()
+    console.log(json.results[0].title);
+    return json.results
+}
+
+const main = async () => {
+    const popularMoviesInformation = await getRequest2();
+    console.log(popularMoviesInformation);
+    const fragment = document.createDocumentFragment();
+    popularMoviesInformation.forEach(movieInformation => {
+        const { poster_path, original_title } = movieInformation;
+        console.log(original_title)
+        console.log(movieInformation)
+        const imageMovie = document.createElement('img');
+        const card = document.createElement('div');
+        // imageMovie.src = `https://image.tmdb.org/t/p/w500${poster_path}`;
+        imageMovie.src = `https://image.tmdb.org/t/p/original${poster_path}`;
+        imageMovie.alt = original_title;
+        imageMovie.className = 'image_movie'
+        card.appendChild(imageMovie);
+        card.className = "card"
+        fragment.appendChild(card);
+    });
+    document.getElementById('movies_grid').appendChild(fragment);
+    // document.body.appendChild(fragment);
+}
+main()
